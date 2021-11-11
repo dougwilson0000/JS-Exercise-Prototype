fdcd1b1983b813b68ab66992e7c97e00545f87e5
Douglas Wilson <dougwilson0000@gmail.com>
2021-11-11 09:15:34 -0500

    finished task 3

--- index.js
+++ index.js
@@ -101,10 +101,26 @@ console.log(Honda);
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this,name, age, favoriteToy);
+  this.favoriteToy = favoriteToy
+
+}
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}`
 }
 
+
+const Arlo = new Baby('Arlo', 3, 'blocks');
+console.log(Arlo);
+
+Arlo.eat('apple')
+Arlo.eat('blueberries');
+console.log(Arlo.stomach);
+Arlo.poop();
+console.log(Arlo.stomach);
+console.log(Arlo.play());
 
 /* 
   TASK 4
