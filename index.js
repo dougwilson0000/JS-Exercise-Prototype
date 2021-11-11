140b5f10db504dfcd3e09cf947de5ffeb41b9ced
Douglas Wilson <dougwilson0000@gmail.com>
2021-11-11 08:42:06 -0500

    completed task 2

--- index.js
+++ index.js
@@ -39,16 +39,32 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name,
+  this.age = age,
+  this.stomach = []
 }
 
+Person.prototype.eat = function(food) {
+  if(this.stomach.length < 10) {
+    this.stomach.push(food);
+  }
+};
 
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
 
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
+}
 
+const Fresh = new Person('Douglas', 25);
 
-
-
+Fresh.eat('tacos');
+console.log(Fresh.stomach);
+Fresh.poop();
+console.log(Fresh.stomach);
 /*
   TASK 2
     - Write a Car constructor that initializes `model` and `milesPerGallon` from arguments.
@@ -63,10 +79,20 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon,
+  this.tank = 0,
+  this.odometer = 0
 }
 
+Car.prototype.fill = function(gallons) {
+  this.tank = this.tank + gallons
+}
+
+const Honda = new Car('Civic', 25);
+Honda.fill(50);
+console.log(Honda);
 
 /*
   TASK 3
